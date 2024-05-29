757533b7203fe7dcec02b49cd4573f5a6b1d4ee1
Saif Rahman <srahman@powerley.com>
2024-05-29 13:28:24 +0600

    added svg converted icons

---
+++ app/icons/Dot.jsx
@@ -0,0 +1,6 @@
+const Dot = () => (
+  <svg width={3} height={4} fill="none">
+    <circle cx={1.5} cy={2} r={1.5} fill="#9BA6B2" />
+  </svg>
+)
+export default Dot
---
+++ app/icons/Warning.jsx
@@ -0,0 +1,15 @@
+const Warning = () => (
+  <svg width={20} height={20} fill="none">
+    <rect
+      width={16.667}
+      height={16.667}
+      x={1.667}
+      y={1.667}
+      fill="#F27D00"
+      rx={8.333}
+    />
+    <rect width={2} height={6.667} x={9} y={5.417} fill="#fff" rx={1} />
+    <rect width={2.2} height={2.2} x={8.9} y={12.8} fill="#fff" rx={1.1} />
+  </svg>
+)
+export default Warning
