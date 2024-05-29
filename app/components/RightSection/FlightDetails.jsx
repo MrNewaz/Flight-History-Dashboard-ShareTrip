1a11988e986ef8d46cb790a39c20433f5381ec79
Saif Rahman <srahman@powerley.com>
2024-05-29 13:34:37 +0600

    Flight details added

---
+++ app/components/RightSection/FlightDetails.jsx
@@ -0,0 +1,15 @@
+import Departure from "./Departure"
+import Destination from "./Destination"
+import Layover from "./Layover"
+
+const FlightDetails = () => {
+  return (
+    <>
+      <Departure />
+      <Layover />
+      <Destination />
+    </>
+  )
+}
+
+export default FlightDetails
